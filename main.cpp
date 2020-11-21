6b2818f9faf7cd52ab26e047ff7ed6e9a0575ae0
alex-stout <stoutab4384@uwec.edu>
2020-11-21 15:37:45 -0600

    finish setup

--- main.cpp
+++ main.cpp
@@ -4,26 +4,64 @@
 
 using namespace std;
 
+// intialize global setting for verbosity as 0, if enabled it is set to 1
+int g_verbose = 0;
+
+int client()
+{
+    cout << "Running as client." << endl;
+    return 0;
+}
+
+int server()
+{
+    cout << "Running as server." << endl;
+    int serve_sock, client_sock, client_addr_size;
+    return 0;
+}
+
+// handle the intial setup
 int main(int argc, char *argv[])
 {
     cout << "number of args " << argc << endl;
     // check to see the number of args. It should be over 2 or 3
     if (argc <= 1)
     {
-        cout << "No arguements were given. Please refer to the README for instructions on how to use this program.";
+        cout << "No arguements were given. Please refer to the README for instructions on how to use this program." << endl;
         exit(1);
     }
-    int type = getType();
-    int results = encrypt(5, 5);
-    cout << results << endl;
-}
+    else if (argc > 3)
+    {
+        cout << "Too many arguements were given. Please refer to the README for instructions on how to use this program." << endl;
+    }
+    else if (argc == 3) // if there's three arguments it means they are including a verbose mode
+    {
+        if (strcmp(argv[2], "-v") == 0 | strcmp(argv[2], "--verbose") == 0)
+        {
+            g_verbose = 1;
+            exit(1);
+        }
+        else
+        {
+            cout << "Unrecognized arguement given. Please refer to the README for instructions on how to use this program." << endl;
+            exit(1);
+        }
+    }
 
-int client()
-{
-    cout << "Running as client.";
-}
-
-int server()
-{
-    cout << "Running as server.";
+    // verify the vailidty of the mode setting then set the mode
+    if (strcmp(argv[1], "client") == 0)
+    {
+        client();
+    }
+    else if (strcmp(argv[1], "server") == 0)
+    {
+        server();
+        system("md5 test.txt");
+    }
+    else
+    {
+        cout << "Unrecognized mode set. Please refer to the README for instructions on how to use this program." << endl;
+        exit(1);
+    }
+    return 0;
 }
